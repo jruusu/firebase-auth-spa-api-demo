847f7769607bed4fe7211549790edbcf6f72ba12
Jan Ruusuvuori <jan.ruusuvuori@kodan.fi>
2018-10-27 18:31:04 +0300

    expose config vars from the server via /config endpoint

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || '3000'
 
+app.get('/config', (req, res) => res.json({
+  firebase: {
+    webApiKey: process.env.FIREBASE_WEB_API_KEY,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+  }
+}))
+
 app.use(express.static('public'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
